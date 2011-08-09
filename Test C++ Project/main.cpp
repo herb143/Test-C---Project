fc90fce8edeaea2206aead174f4c682f6e45fa88
herb143 <h.herbie@gmail.com>
2011-08-09 09:07:43 +0200

    Initial Commit

---
+++ Test C++ Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Test C++ Project
+//
+//  Created by Herbert Gilman on 8/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
